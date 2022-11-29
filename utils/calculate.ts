c967061912c5fec91ff4da0d854bf413917ddba0
Omer <omeryariv@gmail.com>
2022-11-29 16:43:32 -0500

    util library in TS

---
+++ utils/calculate.ts
@@ -0,0 +1,16 @@
+export const add = (first: number, second: number): number => {
+  return first + second;
+};
+
+export const subtract = (first: number, second: number): number => {
+  return first - second;
+};
+
+export const multiply = (first: number, second: number): number => {
+  return first * second;
+};
+
+export const divide = (first: number, second: number): number => {
+  return first / second;
+};
+
